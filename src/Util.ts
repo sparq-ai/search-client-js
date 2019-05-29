bde509a7b16fbf1c495248a4ccd28e7b04cc6051
asinghall <asinghal@greenapplesolutions.com>
2019-05-29 18:29:59 +0530

    Added Server Mapper

---
+++ src/Util.ts
@@ -0,0 +1,13 @@
+export function getServers(appId?: string): { search: string, tracking: string } {
+  let search = `https://${appId}-fast.searchtap.net/v2`;
+  let tracking = `https://t.searchtap.net/v2`;
+  if ((typeof window !== 'undefined') && window.location.host === "beta-web.searchtap.net") {
+    search = 'http://beta-api.searchtap.net/v2/search';
+    tracking = 'http://localhost:3004'
+  }
+
+  return {
+    search: search,
+    tracking: tracking
+  }
+}
