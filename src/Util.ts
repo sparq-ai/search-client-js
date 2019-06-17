83a12857c0190896136919269d1fdb7da9a1e328
asinghall <asinghal@greenapplesolutions.com>
2019-06-17 12:55:04 +0530

    Tracking Host

--- src/Util.ts
+++ src/Util.ts
@@ -1,9 +1,9 @@
 export function getServers(appId?: string): { search: string, tracking: string } {
   let search = `https://${appId}-fast.searchtap.net/v2`;
   let tracking = `https://t.searchtap.net/v2`;
-  if ((typeof window !== 'undefined') && window.location.host === "beta-web.searchtap.net") {
-    search = 'http://beta-api.searchtap.net/v2/search';
-    tracking = 'http://localhost:3004';
+  if ((typeof window !== 'undefined') && window.location.hostname === "139.59.48.135") {
+    search = 'http://139.59.93.13:9000/v2/search';
+    tracking = 'http://139.59.52.249/v2';
   }
 
   return {
